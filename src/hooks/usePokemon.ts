cef32f1d8cedc47bbd84336579a050e8864d6ec6
AndreyrbSilva <andreyrdh@gmail.com>
2026-03-31 23:16:10 -0300

    feat: hooks usePokemon, usePokemonSpecies e usePokemonList

---
+++ src/hooks/usePokemon.ts
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import type { Pokemon, PokemonSpecies } from '../types/pokemon'
+
+export function usePokemon( idOrName: number | string ){
+    const[pokemon, setPokemon] = useState<Pokemon | null>(null)
+    const[loading, setLoading] = useState(true)
+    const[error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${idOrName}`)
+      .then((res) => {
+        if (!res.ok) throw new Error('Pokémon não encontrado')
+        return res.json()
+      })
+      .then((data) => {
+        setPokemon(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err.message)
+        setLoading(false)
+      })
+  }, [idOrName]) // roda de novo sempre que o id mudar
+
+  return { pokemon, loading, error }
+}
+
+export function usePokemonSpecies( idOrName: number | string ){
+    const[species, setSpecies] = useState<PokemonSpecies | null>(null)
+    const[loading, setLoading] = useState(true)
+    const[error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${idOrName}`)
+      .then((res) => {
+        if (!res.ok) throw new Error('Espécie não encontrada')
+        return res.json()
+      })
+      .then((data) => {
+        setSpecies(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err.message)
+        setLoading(false)
+      })
+  }, [idOrName]) // roda de novo sempre que o id mudar
+
+  return { species, loading, error };
+}
+
+export function usePokemonList( offset: number, limit: number ){
+    const[list, setList] = useState<{ name: string; url: string }[]>([])
+    const[total, setTotal] = useState(0)
+    const[loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+          .then(data => data.json())
+            .then(data => {
+                setList(data.results)
+                setTotal(data.count)
+                setLoading(false)
+            })
+    }, [offset, limit])
+
+    return { list, total, loading };
+}
