62be5cefd5593bdaee134fa6f0341b73ca187488
AndreyrbSilva <andreyrdh@gmail.com>
2026-04-03 17:12:34 -0300

    feat: PokemonDetailPage mostrando imagem e dados básicos

--- src/pages/PokemonDetailPage.tsx
+++ src/pages/PokemonDetailPage.tsx
@@ -1,7 +1,22 @@
+import { useParams } from 'react-router-dom'
+import { usePokemon } from "../hooks/usePokemon"
+
 function PokemonDetailPage() {
+    const { id } = useParams()
+    const { pokemon, loading, error } = usePokemon(id ?? '')
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error: {error}</p>
+    if (!pokemon) return <p>Pokémon não encontrado</p>
+
     return  (
         <div>
             <h1>Pokemon Detail</h1>
+            <img
+                src={pokemon.sprites.other["official-artwork"].front_default}
+                alt={pokemon.name}
+            />
+            <h2>{pokemon.name}</h2>
+            <p>Number: {pokemon.id}</p>
         </div>
     )
 }
