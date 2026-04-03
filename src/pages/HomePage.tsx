8543e1b23791724b28873cd5e383a81e3ee6c20a
AndreyrbSilva <andreyrdh@gmail.com>
2026-04-03 18:06:24 -0300

    feat: paginação na HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
 import PokemonCard from "../components/PokemonCard"
 import { usePokemonList } from "../hooks/usePokemon"
+import { usePokedexStore } from '../store/pokedexStore'
 
 export function HomePage() {
-  const { list, total, loading } = usePokemonList(0, 24)
+  const { currentPage, setCurrentPage } = usePokedexStore()
+  const { list, total, loading } = usePokemonList(currentPage * 24, 24)
 
   if (loading) return <p>Loading...</p>
 
@@ -16,6 +18,30 @@ export function HomePage() {
 
           return <PokemonCard key={id} id={Number(id)} />
         })}
+      </div>
+      <div className="flex justify-center mt-6">
+        {currentPage > 0 && (
+          <button onClick={() => setCurrentPage(0)}>«</button>
+        )}
+
+        <button
+          className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 0}
+        >
+          &lt;
+        </button>
+        <span className="mx-4">
+          Page {currentPage + 1} of {Math.ceil(total / 24)}
+        </span>
+        <button
+          className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={(currentPage + 1) * 24 >= total}
+        >
+          &gt;
+        </button>
+        <button onClick={() => setCurrentPage(Math.ceil(total / 24) - 1)}>»</button>
       </div>
     </div>
   )
