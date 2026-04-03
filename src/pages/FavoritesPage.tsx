e30c9df8f8d8b201e7a8688c243ecaf933d4d1c9
AndreyrbSilva <andreyrdh@gmail.com>
2026-04-03 17:41:06 -0300

    feat: FavoritesPage mostrando pokémons favoritados

--- src/pages/FavoritesPage.tsx
+++ src/pages/FavoritesPage.tsx
@@ -1,7 +1,17 @@
+import { usePokedexStore } from '../store/pokedexStore'
+import PokemonCard from '../components/PokemonCard'
+
 function FavoritesPage() {
+  const { favorites } = usePokedexStore()
+
   return (
     <div>
       <h1>Favorites</h1>
+      <div>
+        {favorites.map((id) => (
+          <PokemonCard key={id} id={id} />
+        ))}
+      </div>
     </div>
   )
 }
